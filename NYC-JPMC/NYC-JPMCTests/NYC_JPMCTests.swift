dfdb47f3671c33e4b6fc26b634a02c6f2fccd9a9
Osama Inam <osamainam777@users.noreply.github.com>
2023-01-11 00:48:11 -0600

    Final commit

--- NYC-JPMC/NYC-JPMCTests/NYC_JPMCTests.swift
+++ NYC-JPMC/NYC-JPMCTests/NYC_JPMCTests.swift
@@ -6,9 +6,12 @@
 //
 
 import XCTest
+import SwiftUI
 @testable import NYC_JPMC
 
 final class NYC_JPMCTests: XCTestCase {
+    
+    @ObservedObject var viewModel: SchoolViewModel = SchoolViewModel()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -32,5 +35,31 @@ final class NYC_JPMCTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+    
+    func testFetchSchool() {
+        let expectation = XCTestExpectation(description: "Must return true")
+        let viewModel = viewModel
+        viewModel.getData { success in
+            if success {
+                expectation.fulfill()
+                
+            } else {
+                XCTFail("Couldn't get data")
+            }
+        }
+    }
+    
+    func testGetScore() {
+        let expectation = XCTestExpectation(description: "Must return true")
+        let viewModel = viewModel
 
+        viewModel.getSatData(with: "21K728") { success in
+            if success {
+                expectation.fulfill()
+                
+            } else {
+                XCTFail("Couldn't get data")
+            }
+        }
+    }
 }
